9e3f28f0299096d6f0e059fb2b014700b76df705
Amine Aouragh <112860666+Zrynix@users.noreply.github.com>
2024-12-08 19:48:41 +0100

    reserveringweergave

--- Code/reserveringweergave.php
+++ Code/reserveringweergave.php
@@ -55,7 +55,7 @@ try {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="styles.css">
-    <title>Admin Reserveringen</title>
+    <title>Admin Reservering</title>
     
 </head>
 <body class="bg-gray-100">
